1b975a0b240827081bf7f459e0aa2c14a51b5614
cinamonview <cinamonview@gmail.com>
2026-05-27 15:18:36 +0900

    푸시

---
+++ src/banking1/AccountManager.java
@@ -0,0 +1,12 @@
+package banking1;
+
+public class AccountManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private Account[] accountArray = new Account[50];
+		
+		private int accountCount = 0;
+	}
+
+}
