c747b3d192814e3478c3b03f11a1a5d00e2beb50
cinamonview <cinamonview@gmail.com>
2026-05-29 10:51:35 +0900

    순번적용함

--- src/banking2/BankingSystemMain.java
+++ src/banking2/BankingSystemMain.java
@@ -19,8 +19,11 @@ public class BankingSystemMain {
         System.out.print("선택:");
     }
     
-    //학원에서 pull없이 작업함
-    //github clone 작업 성공
+    //1github연동
+    //2연동완료후 커밋&푸시
+    //3추가작업
+    //4학원에서 pull없이 작업함
+    //5github clone 작업 성공
 	public static void main(String[] args) {
 		
 		//main에서 메니져 인스턴스 생성시 배열의 크기 50을 전달 
